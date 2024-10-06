d7cdf6322e815accd0df8801d7dea49f776ea897
Ricardo Pinheiro <rpinheiro.033@gmail.com>
2024-10-06 01:07:55 +0100

    Add playGame function to keep track of the game with cpu

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 const MIN_RANDOM = 0;
 const MAX_RANDOM = 3;
+const ROUND_LIMIT = 5;
+
+let humanScore = 0;
+let cpuScore = 0;
 
 let getComputerChoice = () => {
     let intermediateRslt = Math.floor(Math.random() * (MAX_RANDOM - MIN_RANDOM) + MIN_RANDOM);
@@ -26,7 +30,6 @@ let getComputerChoice = () => {
 let getHumanChoice = () => {
     let humanPrompt = prompt("What is your guess?");
     let humanChoice;
-    console.log("Second time?");
     switch(humanPrompt) {
         case "rock":
             humanChoice = "rock";
@@ -46,5 +49,49 @@ let getHumanChoice = () => {
 } // Still has a bug if we choose a different input than the accepted ones.
 
 
-let resultFromCPUComputation = document.getElementById("computerResult");
-resultFromCPUComputation.textContent = "Computer result is: " + getComputerChoice();
+function playRound(humanChoice, cpuChoice) {
+    if(humanChoice === cpuChoice) {
+        console.log("It's a tie!");
+        humanScore++;
+        cpuScore++;
+    } else if(humanChoice === "rock" && cpuChoice === "paper") {
+        console.log("You lose! Paper beats rock.");
+        cpuScore++;
+    } else if(humanChoice === "paper" && cpuChoice === "scissors") {
+        console.log("You lose! Scissors beats paper.");
+        cpuScore++;
+    } else if(humanChoice === "scissors" && cpuChoice === "paper") {
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    } else if(humanChoice === "rock" && cpuChoice === "scissors") {
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    } else if(humanChoice === "paper" && cpuChoice === "rock") {
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    } else if(humanChoice === "scissors" && cpuChoice === "rock") {
+        console.log("You lose! Scissors beats rock.");
+        cpuScore++;
+    } 
+}
+
+
+function playGame() {
+    let currentRound = 0;
+    while(currentRound < ROUND_LIMIT) {
+        const humanSelection = getHumanChoice();
+        const cpuSelection = getComputerChoice();
+        playRound(humanSelection, cpuSelection);
+        currentRound++;
+    }
+    if(cpuScore > humanScore) {
+        console.log("You lost!");
+    } else {
+        console.log("You win!");
+    }
+}
+
+let btnStartGame = document.getElementById("startGame");
+btnStartGame.addEventListener("click", () => {
+    playGame();
+});
